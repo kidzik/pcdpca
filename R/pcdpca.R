fd91337c98e8535ae3bf1470865b90cf2613d68a
Lukasz Kidzinski <lukasz.kidzinski@gmail.com>
2016-10-16 18:37:42 -0700

    The first commit

---
+++ R/pcdpca.R
@@ -0,0 +1,11 @@
+#' Compute periodically correlated DPCA filters and scores
+#'
+#' @param X multivariate time series
+#' @keywords pcdpca
+#' @export
+#' @examples
+#' pcdpca(X)
+ 
+pcdpca <- function(X){
+
+}
